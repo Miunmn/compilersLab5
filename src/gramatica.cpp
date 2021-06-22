bf098792a2b49829437dd1522f1c7264cc48eb6d
miunmnn <esteban.principe@utec.edu.pe>
2021-06-21 19:01:45 -0500

    Update gramatica

--- src/gramatica.cpp
+++ src/gramatica.cpp
@@ -537,25 +537,3 @@ public:
 
 
     };
-
-class Parser{
-    FlexLexer* lexer;
-    
-    public:
-    void parse(){
-        //Consultar tabla LL1
-        //Consultar teglas gramaticales
-        while(lexer->yylex() != 0);
-    }
-
-   Parser(){
-       lexer = new yyFlexLexer;
-   }
-};
-
-int main(){
-    Parser p;
-    p.parse();
-
-    return 0;
-}
